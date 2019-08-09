302688daff7c30b4ca250b27b8e62f75b0e58a7e
Diego Cezar <diegocezar.comercial@gmail.com>
2019-08-09 00:38:38 -0300

    Arquitetura de pastas usando ITCSS

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h2 className="about">Sobre</h2>
-      </header>
-    </div>
+    <a className="about" href="#to-do">
+      Sobre
+    </a>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/settings/colors.css";
+import "./styles/generic/reset.css";
+import "./styles/elements/base.css";
 
+import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
+ReactDOM.render(<App />, document.getElementById("root"));
