136721562136c5164c63ed782c03b183c7d12d3f
Diego Cezar <diegocezar.comercial@gmail.com>
2019-08-10 17:24:15 -0300

    Aula 49 desafio fácil - Adicionar logo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import Sobre from "./objects/Sobre";
+import Logo from "./objects/Logo";
 
 function App() {
-  return <Sobre />;
+  return (
+    <div>
+      <Logo />
+      <Sobre />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/objects/Logo/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "./styles.css";
+
+const Logo = () => {
+  return (
+    <a href="#to-do" className="logo">
+      Collabcode
+    </a>
+  );
+};
+export default Logo;
