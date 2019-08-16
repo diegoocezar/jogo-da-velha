43bc5746d1e53e4ffc0849cc26af31a464e104b9
Diego Cezar <diegocezar.comercial@gmail.com>
2019-08-15 23:59:38 -0300

    Aula 52 - Criados label e input

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
 import "./App.css";
+
 import Header from "./components/Header";
 import Hashtag from "./components/Hashtag";
+import Label from "./objects/Label";
+import Input from "./objects/Input";
 
 const App = () => (
   <main className="app">
     <Header />
     <Hashtag />
+    <Input value="show" />
+    <Label content="Mostrar eventos" />
   </main>
 );
 
---
+++ src/objects/Label/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./styles.css";
+
+const Label = ({ content }) => (
+  <label htmlFor="" className="label">
+    {content}
+  </label>
+);
+
+export default Label;
