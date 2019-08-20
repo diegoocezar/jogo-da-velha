f75945b2833e34bdae5e48b41e5a30a139a26e0b
Diego Cezar <diegocezar.comercial@gmail.com>
2019-08-20 17:54:02 -0300

    Aula 52 básico - criar tela de sobre com cor

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import "./App.css";
 import Header from "./components/Header";
 import Hashtag from "./components/Hashtag";
 import Checkbox from "./objects/Checkbox";
+import About from "./objects/About";
 
 const App = () => (
   <main className="app">
     <Header />
+    <About />
     <Hashtag />
     <Checkbox value="show" id="show" content="Mostrar eventos" />
   </main>
---
+++ src/objects/About/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+import "./styles.css";
+// import Header from "../../components/Header";
+
+const About = () => <article className="about">{/* <Header /> */}</article>;
+
+export default About;
