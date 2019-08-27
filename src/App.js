bd654c7cdcb5d491a94473ae2a38f0a8c4fac195
Diego Cezar <diegocezar.comercial@gmail.com>
2019-08-26 22:27:45 -0300

    Aula 53 - HeaderInternal criado e posicionado

--- src/App.js
+++ src/App.js
@@ -3,12 +3,10 @@ import "./App.css";
 
 import Checkbox from "./objects/Checkbox";
 import About from "./objects/About";
-import AboutLink from "./objects/AboutLink";
-import IconClose from "./objects/IconClose";
-import LogoCollab from "./objects/LogoCollab";
 
 import Header from "./components/Header";
 import Hashtag from "./components/Hashtag";
+import HeaderInternal from "./components/HeaderInternal";
 
 const App = () => (
   <main className="app">
@@ -16,9 +14,7 @@ const App = () => (
     <Hashtag />
     <Checkbox value="show" id="show" content="Mostrar eventos" />
     <About>
-      <LogoCollab light />
-      <AboutLink className="-light" />
-      <IconClose />
+      <HeaderInternal />
     </About>
   </main>
 );
---
+++ src/components/HeaderInternal/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./styles.css";
+
+import AboutLink from "../../objects/AboutLink";
+import IconClose from "../../objects/IconClose";
+import LogoCollab from "../../objects/LogoCollab";
+
+const HeaderInternal = () => (
+  <header className="header-internal">
+    <LogoCollab light />
+    <AboutLink className="-light" />
+    <IconClose />
+  </header>
+);
+
+export default HeaderInternal;
